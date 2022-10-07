f7e2f4d73bdedfce358ff2054174327611467150
Blake Simpson <blakesimpson.dev@outlook.com>
2022-10-07 13:22:47 +0800

    fixed netlify function

--- netlify/functions/emailjs.js
+++ netlify/functions/emailjs.js
@@ -2,9 +2,9 @@ exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      serviceId: process.env.EMAILJS_SERVICE_ID || '',
-      tempalteId: process.env.EMAILJS_TEMPLATE_ID || '',
-      publicKey: process.env.EMAILJS_PUBLIC_KEY || '',
+      serviceId: process.env.EMAILJS_SERVICE_ID,
+      tempalteId: process.env.EMAILJS_TEMPLATE_ID,
+      publicKey: process.env.EMAILJS_PUBLIC_KEY,
     }),
   }
 }
