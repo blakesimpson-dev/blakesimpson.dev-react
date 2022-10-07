f4b92cb76b2666505157c07c3a8a7c349d10f83b
Blake Simpson <blakesimpson.dev@outlook.com>
2022-10-07 15:26:06 +0800

    reworking contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,31 +1,53 @@
-import React, { useRef, useState } from 'react'
+// import React, { useRef, useState } from 'react'
+import React from 'react'
 import { FaEnvelope } from 'react-icons/fa'
 import '../styles/contact-form.scss'
 
 const ContactForm = () => {
-  const form = useRef()
-  const [isSubmitted, setSubmitted] = useState(false)
+  // const form = useRef()
+  // const [isSubmitted, setSubmitted] = useState(false)
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
 
-    setTimeout(() => {
-      setSubmitted(true)
-    }, 100)
-  }
+  //   setTimeout(() => {
+  //     setSubmitted(true)
+  //   }, 100)
+  // }
 
   return (
     <form
-      name="contactForm"
-      className="contact-form"
-      ref={form}
-      onSubmit={handleSubmit}
+      name="contact"
       method="POST"
-      target="_blank"
       data-netlify="true"
-      netlify-honeypot="bot-field"
+      target="_blank"
+      className="contact-form"
     >
-      {isSubmitted && (
+      <input type="hidden" name="form-name" value="contact" />
+      <h1>Send me a message</h1>
+      <div className="contact-form__input">
+        <label htmlFor="name">
+          Your name<em>*</em>
+        </label>
+        <input id="name" name="name" type="text" autoFocus required />
+      </div>
+      <div className="contact-form__input">
+        <label htmlFor="email">
+          Your email<em>*</em>
+        </label>
+        <input id="email" name="email" type="email" required />
+      </div>
+      <div className="contact-form__textarea">
+        <label htmlFor="message">
+          Message<em>*</em>
+        </label>
+        <textarea id="message" name="message" required />
+      </div>
+      <button type="submit">
+        <FaEnvelope />
+        &nbsp;Send
+      </button>
+      {/* {isSubmitted && (
         <span>Thank you for reaching out to me. I will be in touch soon.</span>
       )}
       {!isSubmitted && (
@@ -55,7 +77,7 @@ const ContactForm = () => {
             &nbsp;Send
           </button>
         </>
-      )}
+      )} */}
     </form>
   )
 }
